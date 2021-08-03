56619131841197351198eb2865b9b2d526f3667f
RainyLofi <71572372+RainyLofi@users.noreply.github.com>
2021-08-04 00:53:27 +0100

    Update SafeTeleport.lua

--- Modules/SafeTeleport.lua
+++ Modules/SafeTeleport.lua
@@ -1,22 +1,14 @@
-local HumanoidConnections = {'Changed', 'StateChanged'}
+local Parts = {}
 
 return function(Character, CF)
 	for _, Child in pairs(Character:GetChildren()) do
-		if Child:IsA('BasePart') then
+		if Child:IsA('BasePart') and not table.find(Parts, Child) then
 			for _, Connection in pairs(getconnections(Child.Changed)) do
 				Connection:Disable()
 			end
+			table.insert(Parts, Child)
 		end
 	end
-
-    local Humanoid = Character:FindFirstChildOfClass('Humanoid')
-    --[[if Humanoid then
-        for _, Con in pairs(HumanoidConnections) do
-            for _, Connection in pairs(getconnections(Humanoid[Con])) do
-                Connection:Disable()
-            end
-        end
-    end]]--
 
 	Character.PrimaryPart.CFrame = CF
 	Character:SetPrimaryPartCFrame(CF)
