6996829fc299625ec6da756f63fd37e2f26790b1
RainyLofi <71572372+RainyLofi@users.noreply.github.com>
2021-08-06 16:42:18 +0100

    rainbow bullets

--- init.client.lua
+++ init.client.lua
@@ -29,6 +29,8 @@ local Entities = Workspace:WaitForChild('Entities')
 local EntityObjectives = Entities:WaitForChild('Objectives')
 local Infected = Entities:WaitForChild('Infected')
 
+local RE = ReplicatedStorage:WaitForChild('RE')
+
 ----------------------------------------------
 --// Import function
 
@@ -125,6 +127,22 @@ if not RunService:IsStudio() then
 		if NamecallMethod == 'FireServer' then
 			if Args[2] and Args[2] == 'RejoinK' then spawn(Rejoin) return wait(9e9) end
 			if Args[2] and Args[2] == 'CheatKick' then spawn(Rejoin) return wait(9e9) end
+			if Args[2] and Args[2] == 'GlobalReplicate' and Args[3] and type(Args[3]) == 'table' then
+				if Args[3]['Type'] and Args[3]['Type'] == 'Fire' then
+					spawn(function() -- rainbow bullets
+						RE:FireServer(
+							"EditData",
+							{
+								{
+									["Option"] = "TracerNum",
+									["Type"] = "EditOptions",
+									["Value"] = math.random(2, 14)
+								}
+							}
+						)
+					end)
+				end
+			end
 		elseif NamecallMethod == 'Kick' then
 			spawn(Rejoin)
 			return wait(9e9)
